0fb1f628280eed8877335e30135980601ac213b1
Florian Patruck <florian.patruck@hdnet.de>
2021-12-06 10:07:25 +0100

    Chore(form): Install symfony form component
    Relates to AUP-261

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitc61324bf58a007c1c00bd0c479949ba6
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitc61324bf58a007c1c00bd0c479949ba6', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc61324bf58a007c1c00bd0c479949ba6', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
